391cb51dca2fd82fdc2429275c0c705930fc9cd3
EthanLiu1991 <liuliu1991@tongji.edu.cn>
2021-11-30 18:03:43 +0800

    modify rpc

--- torch/utils/data/proto/variablebatch_pb2_grpc.py
+++ torch/utils/data/proto/variablebatch_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import variablebatch_pb2 as variablebatch__pb2
+from . import variablebatch_pb2 as variablebatch__pb2
 
 
 class VariableBatchStub(object):
@@ -15,10 +15,10 @@ class VariableBatchStub(object):
             channel: A grpc.Channel.
         """
         self.GetVariableBatch = channel.unary_unary(
-                '/VariableBatch/GetVariableBatch',
-                request_serializer=variablebatch__pb2.GetVariableBatchRequest.SerializeToString,
-                response_deserializer=variablebatch__pb2.GetVariableBatchReply.FromString,
-                )
+            '/VariableBatch/GetVariableBatch',
+            request_serializer=variablebatch__pb2.GetVariableBatchRequest.SerializeToString,
+            response_deserializer=variablebatch__pb2.GetVariableBatchReply.FromString,
+        )
 
 
 class VariableBatchServicer(object):
@@ -33,34 +33,35 @@ class VariableBatchServicer(object):
 
 def add_VariableBatchServicer_to_server(servicer, server):
     rpc_method_handlers = {
-            'GetVariableBatch': grpc.unary_unary_rpc_method_handler(
-                    servicer.GetVariableBatch,
-                    request_deserializer=variablebatch__pb2.GetVariableBatchRequest.FromString,
-                    response_serializer=variablebatch__pb2.GetVariableBatchReply.SerializeToString,
-            ),
+        'GetVariableBatch': grpc.unary_unary_rpc_method_handler(
+            servicer.GetVariableBatch,
+            request_deserializer=variablebatch__pb2.GetVariableBatchRequest.FromString,
+            response_serializer=variablebatch__pb2.GetVariableBatchReply.SerializeToString,
+        ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
-            'VariableBatch', rpc_method_handlers)
+        'VariableBatch', rpc_method_handlers)
     server.add_generic_rpc_handlers((generic_handler,))
 
-
  # This class is part of an EXPERIMENTAL API.
+
+
 class VariableBatch(object):
     """Missing associated documentation comment in .proto file."""
 
     @staticmethod
     def GetVariableBatch(request,
-            target,
-            options=(),
-            channel_credentials=None,
-            call_credentials=None,
-            insecure=False,
-            compression=None,
-            wait_for_ready=None,
-            timeout=None,
-            metadata=None):
+                         target,
+                         options=(),
+                         channel_credentials=None,
+                         call_credentials=None,
+                         insecure=False,
+                         compression=None,
+                         wait_for_ready=None,
+                         timeout=None,
+                         metadata=None):
         return grpc.experimental.unary_unary(request, target, '/VariableBatch/GetVariableBatch',
-            variablebatch__pb2.GetVariableBatchRequest.SerializeToString,
-            variablebatch__pb2.GetVariableBatchReply.FromString,
-            options, channel_credentials,
-            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+                                             variablebatch__pb2.GetVariableBatchRequest.SerializeToString,
+                                             variablebatch__pb2.GetVariableBatchReply.FromString,
+                                             options, channel_credentials,
+                                             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
